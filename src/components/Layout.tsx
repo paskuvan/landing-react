bf5247ad941e2d48604f042e5c3e2b12d4aede2d
paskuvan <majopaskuvan@gmail.com>
2025-04-13 22:59:59 -0400

    second commit

---
+++ src/components/Layout.tsx
@@ -0,0 +1,25 @@
+import { Navbar } from "./elements/Navbar";
+import { Footer } from "./elements/Footer";
+
+interface LayoutProps {
+    title: string;
+    children: React.ReactNode;
+}
+
+export const Layout =({title, children}: LayoutProps) => {
+    return (
+    <> 
+    
+        <Navbar />
+
+        <main className="flex flex-col gap-y-20 md:gap-y-32 overflow-hidden">
+        {children}
+        </main>
+
+        
+
+        <Footer />
+    
+    </>
+    );
+};
