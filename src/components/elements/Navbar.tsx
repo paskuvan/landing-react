e0fb3e394b160a1a4774766ee81f8c7f5f65b718
paskuvan <majopaskuvan@gmail.com>
2025-04-15 23:12:24 -0400

    end

--- src/components/elements/Navbar.tsx
+++ src/components/elements/Navbar.tsx
@@ -1,6 +1,4 @@
 import { Container } from "../shared/Container";
-import logo from "../../assets/logo.svg";
-
 
 
 const navItems = [
@@ -16,12 +14,9 @@ export const Navbar = () => {
             <nav className="w-full flex justify-between gap-6 relative">
                
             <div className="min-w-max inline-flex relative">
-            <a href="/" className="relative flex items-center gap-3">
-              <img src={logo} alt="EdgeAI Logo" className="w-10 h-10" />
-              <div className="inline-flex text-lg font-semibold text-heading-1">
-                EdgeAI
-              </div>
-            </a>
+            <div className="inline-flex text-lg font-semibold text-heading-1">
+                  EdgeAI
+                </div>
           </div>
             </nav>
         </Container>
